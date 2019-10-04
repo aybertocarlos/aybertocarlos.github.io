a32d538fa179e938b21a2e04420dfec9781b3324
aybertocarlos <aybertocarlos@gmail.com>
2019-10-04 12:45:01 +0300

    First commit

---
+++ precache-manifest.4d6a2f1e7a1a0f46d732a26ece62dd2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da08c622de461b5dc0c961eb7d4a204",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c30271b68d164633ef7",
+    "url": "/static/css/main.4e0e4d8d.chunk.css"
+  },
+  {
+    "revision": "b9a6560dccbeb9e108a3",
+    "url": "/static/js/2.edb5096e.chunk.js"
+  },
+  {
+    "revision": "0c30271b68d164633ef7",
+    "url": "/static/js/main.0d16ce74.chunk.js"
+  },
+  {
+    "revision": "169e8fec4026b9922026",
+    "url": "/static/js/runtime-main.a5016f14.js"
+  },
+  {
+    "revision": "d3989c88b68d4914654e3b9aaca0bb87",
+    "url": "/static/media/aybertocar.d3989c88.png"
+  }
+]);
