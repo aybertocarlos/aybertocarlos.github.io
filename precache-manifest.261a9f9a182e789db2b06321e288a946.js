b25784415a85d4e4c2aeb4e1503b4f98afbb48a5
Ayberk Tandogan <l5215066@std.yildiz.edu.tr>
2020-02-15 22:33:43 +0300

    New Build

---
+++ precache-manifest.261a9f9a182e789db2b06321e288a946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b15383419e63f6e1f31bbc30736c94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b48898709c88262380",
+    "url": "/static/css/main.4e0e4d8d.chunk.css"
+  },
+  {
+    "revision": "8b8463fd4e5a9234e792",
+    "url": "/static/js/2.fbadaeb8.chunk.js"
+  },
+  {
+    "revision": "72b48898709c88262380",
+    "url": "/static/js/main.2dc6a779.chunk.js"
+  },
+  {
+    "revision": "169e8fec4026b9922026",
+    "url": "/static/js/runtime-main.a5016f14.js"
+  },
+  {
+    "revision": "d3989c88b68d4914654e3b9aaca0bb87",
+    "url": "/static/media/aybertocar.d3989c88.png"
+  }
+]);
